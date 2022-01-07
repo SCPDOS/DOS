5faeaf85004fc402ff462fa59eee773148b41101
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-01-07 12:32:57 +0000

    Changed the BIOS image to the up to date with correct CLD

--- io.asm
+++ io.asm
@@ -1,9 +1,13 @@
+    DEFAULT REL
+    [map all io.map]
     BITS 64
-    ORG 7C00h
-    dw 0AA55h ;Initial signature
+Section loader vstart=7C00h align=1
+    dw 0AA55h           ;Initial signature
+    xchg bx, bx
+    mov ecx, 0C0000100h ;Read fs base into rdx:rax
+    rdmsr
     mov rbp, strng
     mov ax, 1304h
     int 30h
     jmp $
-    
 strng db 0Ah, 0Dh,"Invalid IO.SYS found...",0
