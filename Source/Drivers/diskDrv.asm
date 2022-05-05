a1b0c7817eaa68909c304d59d365782042f5c99c
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-05-05 10:49:30 +0100

    Added partition aware MSD functions

--- Source/Drivers/diskDrv.asm
+++ Source/Drivers/diskDrv.asm
@@ -17,7 +17,7 @@ msdDriver:
     lea rsi, .msdBPBTbl  ;Point to the BPB pointer table
     movzx eax, byte [rbx + drvReqHdr.unitnm]
     shl eax, 3  ;Multiply by 8 to get pointer to pointer to bpb
-    mov rbp, qword [rax]    ;Get pointer to bpb in rbp
+    mov rbp, qword [rsi + rax]    ;Get pointer to bpb in rbp
     movzx eax, byte [rbx + drvReqHdr.cmdcde]   ;Get command code in al
     shl eax, 1  ;Multiply by 2 since each entry is a word in size
     lea rcx, .msdTable
@@ -25,7 +25,7 @@ msdDriver:
     test eax, eax   ;Is the distance 0, i.e. function not implemented?
     jz .msdDriverExit ;Valid function number but not for MSD, exits with done!
     add rax, rcx    ;Else, add table address to the distance from the table
-    call rax ;Goto the function
+    call rax ;Goto function, rbp = devBPBPtr, rbx = reqBlkPtr
 .msdDriverExit:
     or word [rbx + drvReqHdr.status], 0100h ;Set done bit
     pop r8
@@ -38,15 +38,14 @@ msdDriver:
     pop rax
     ret
 
-.msdIOError:  ;In Read and Write errors, rbp points to the dev struc
-    mov rbx, rbp
+.msdIOError:  ;In Read and Write errors, rdi points to the dev struc
+    mov rbx, rdi
     movzx eax, al   ;Number of IO-ed sectors in last request
     add esi, eax    ;esi Keeps sector count across transfers
     mov dword [rbx + ioReqPkt.tfrlen], esi ;Save number of IO-ed sectors
 ;Now fall through to general error
 .msdGenDiskError:   ;DISK DRIVER ERROR HANDLER
     mov ah, 01h
-    xor dl, dl  ;Work around bug that fails request if dl > 7Fh
     int 33h ;Read status of last operation
     cmp ah, 80h ;Timeout/Media Not Ready response (device not present)
     mov al, 02h ;Give device not ready error (sensibly I think)
@@ -128,23 +127,150 @@ msdDriver:
     dw .msdGetLogicalDev - .msdTable    ;Function 23
     dw .msdSetLogicalDev - .msdTable    ;Function 24
 
-
+;All functions have the request packet ptr in rbx and the bpb pointer in rbp
 .msdInit:            ;Function 0
     ret
 .msdMedChk:          ;Function 1
+    mov al, 05h ;Bad request structure length
+    cmp byte [rbx + drvReqHdr.hdrlen], mediaCheckReqPkt_size
+    jne .msdWriteErrorCode
+
+    movzx rax, byte [rbx + mediaCheckReqPkt.unitnm]
+    lea rcx, .msdBIOSmap
+    mov dl, byte [rcx + rax]    ;Translate unitnum to BIOS num
+    test dl, 80h    ;If it is a fixed disk, no change!
+    jnz .mmcNoChange
+;Now we do a BIOS changeline check. If it returns 80h or 86h then check med desc
+    mov ah, 16h 
+    int 33h
+    jc .msdGenDiskError
+    cmp ah, 80h
+    je .mmcNoChangeLine
+    cmp ah, 86h
+    je .mmcNoChangeLine
+    test ah, ah ;No change?
+    jz .mmcNoChange
+    test ah, 1  ;Neither 80h or 86h have bit 0 set
+    jnz .mmcChange
+;If nothing, fall through and test manually, should never happen though
+.mmcNoChangeLine:
+;Now we test Media Descriptor
+    movzx rax, byte [rbx + mediaCheckReqPkt.unitnm]
+    mov dl, byte [rbx + mediaCheckReqPkt.medesc]    ;Media descriptor
+    cmp byte [rbp + bpb32.media], dl    ;Compare media descriptor bytes
+    je .mmcUnsure
+.mmcChange:
+    mov byte [rbx + mediaCheckReqPkt.medret], -1
+    lea rax, .msdDefLabel          ;Temp, ret def label
+    mov qword [rbx + mediaCheckReqPkt.desptr], rax 
     ret
+.mmcUnsure:
+    mov byte [rbx + mediaCheckReqPkt.medret], 0
+    ret
+.mmcNoChange:
+    mov byte [rbx + mediaCheckReqPkt.medret], 1
+    ret
+
+
 .msdBuildBPB:        ;Function 2
+    mov al, 05h ;Bad request structure length
+    cmp byte [rbx + drvReqHdr.hdrlen], bpbBuildReqPkt_size
+    jne .msdWriteErrorCode
+
+    mov rsi, rbx
+    movzx rax, byte [rsi + bpbBuildReqPkt.unitnm]  ;Get unit number into rax
+    lea rcx, .msdBIOSmap
+    mov dl, byte [rcx + rax]  ;Get translated BIOS number for req
+    mov rbx, qword [rsi + bpbBuildReqPkt.bufptr]    ;Transfer buffer
+    xor ecx, ecx    ;Read Sector 0...
+    add ecx, dword [rbp + bpb32.hiddSec]    ;Of selected volume!
+    mov eax, 8201h  ;LBA Read 1 sector
+    int 33h
+    jc .msdGenDiskError
+;Check Media Descriptor, must be F0h or F8h-FFh or unknown media
+    cmp byte [rbx + bpb.media], 0F0h    ;3.5" FDD standard
+    je .mbbpb0
+    cmp byte [rbx + bpb.media], 0F8h    ;FDD/Large Media Standard
+    je .mbbpb0
+    cmp byte [rbx + bpb.media], 0F9h    ;5.25" & 720K 3.5" Media Standard
+    je .mbbpb0
+    cmp byte [rbx + bpb.media], 0FCh    ;Very Obsolete Media Standards
+    mov al, 07h ;Unknown media error code
+    jb .msdWriteErrorCode
+.mbbpb0:
+    xchg rbx, rsi    ;Transf Buf(rbx) <-> ReqHdr(rsi)
+    movzx rax, byte [rbx + bpbBuildReqPkt.unitnm]  ;Get unit number into rax
+    lea rdi, .msdBPBTbl
+    mov rdi, qword [rdi + 8*rax] ;Get pointer to pointer to buffer
+    mov rdi, qword [rdi] ;Dereference to get pointer to buffer 
+    mov qword [rbx + bpbBuildReqPkt.bpbptr], rdi ;rdi -> final bpb resting place
+    mov ecx, bpbEx_size/8
+    rep movsq   ;Move the BPB data into the right space
     ret
 .msdIOCTLRead:       ;Function 3, returns done
+    mov al, drvBadDrvReq
+    cmp byte [rbx + drvReqHdr.hdrlen], ioReqPkt_size
+    jne .msdWriteErrorCode
     ret
 .msdRead:            ;Function 4
 ;Will read one sector at a time.
+    mov al, 05h ;Bad request structure length
+    cmp byte [rbx + drvReqHdr.hdrlen], ioReqPkt_size
+    jne .msdWriteErrorCode
+
+    mov rdi, rbx
+    xor esi, esi  ;Set sector read counter to zero
+.msdr0:
+    mov dh, 82h ;LBA Read Sectors
+    call .msdBlkIOCommon
+    jc .msdIOError
+    add qword [rdi + ioReqPkt.strtsc], 200h  ;Add one sector
+    add qword [rdi + ioReqPkt.bufptr], 200h  ;Add one sector
+    inc esi
+    cmp esi, dword [rdi + ioReqPkt.tfrlen]
+    jne .msdr0
+    mov rbx, rdi
     ret
 .msdWrite:           ;Function 8
 ;Will write one sector at a time.
+    mov al, 05h ;Bad request structure length
+    cmp byte [rbx + drvReqHdr.hdrlen], ioReqPkt_size
+    jne .msdWriteErrorCode
+
+    mov rdi, rbx
+    xor esi, esi  ;Set sector read counter to zero
+.msdw0:
+    mov dh, 83h ;LBA Write Sectors
+    call .msdBlkIOCommon
+    jc .msdIOError
+    add qword [rdi + ioReqPkt.strtsc], 200h  ;Add one sector
+    add qword [rdi + ioReqPkt.bufptr], 200h  ;Add one sector
+    inc esi
+    cmp esi, dword [rdi + ioReqPkt.tfrlen]
+    jne .msdw0
+    mov rbx, rdi
     ret
 .msdWriteVerify:     ;Function 9, writes sectors then verifies them
 ;Will write one sector at a time and then verify it.
+    mov al, 05h ;Bad request structure length
+    cmp byte [rbx + drvReqHdr.hdrlen], ioReqPkt_size
+    jne .msdWriteErrorCode
+
+    mov rdi, rbx
+    xor esi, esi  ;Set counter to zero
+.msdwv0:
+    mov dh, 83h ;LBA Write Sectors
+    call .msdBlkIOCommon
+    jc .msdIOError    ;Error handler needs to add to esi the value in al
+    mov dh, 84h ;LBA Verify Sectors
+    call .msdBlkIOCommon
+    jc .msdIOError    ;Error handler needs to add to esi the value in al
+    add qword [rbp + ioReqPkt.strtsc], 200h  ;Add one sector
+    add qword [rbp + ioReqPkt.bufptr], 200h  ;Add one sector
+    inc esi
+    cmp esi, dword [rbp + ioReqPkt.tfrlen]
+    jne .msdwv0
+    mov rbx, rdi
     ret
 .msdIOCTLWrite:      ;Function 12, returns done
     mov al, drvBadDrvReq
@@ -178,9 +304,11 @@ msdDriver:
     movzx rax, byte [rbx + remMediaReqPkt.unitnm]
     lea rcx, .msdBIOSmap
     mov al, byte [rcx + rax]    ;Get BIOS number
+    mov ecx, 0200h  ;Busy bit set
+    xor edx, edx    ;Busy bit clear
     test al, 80h
-    jz .msdDriverExit   ;If removable, busy bit is clear
-    mov word [rbx + remMediaReqPkt.status], 0200h ;Set Busy bit
+    cmovz ecx, edx
+    mov word [rbx + remMediaReqPkt.status], cx ;Set Busy bit if removable
     ret
 .msdGenericIOCTL:    ;Function 19
     mov al, drvBadDrvReq
@@ -206,14 +334,17 @@ msdDriver:
     ret
 
 .msdBlkIOCommon:  ;Does block IO
-;Called with rbp containing old rbx value and ah with function number
+;Called with rdi containing old rbx value and ah with function number
+;rbp points to bpb
 ;Error handled by caller
 ;Sector count handled by caller
 ;Called with dh = BIOS function number
-    movzx rax, byte [rbp + ioReqPkt.unitnm]
+    movzx rax, byte [rdi + ioReqPkt.unitnm]
     mov dl, byte [.msdBIOSmap + rax]  ;Get translated BIOS number for req in dl
-    mov rcx, qword [rbp + ioReqPkt.strtsc]  ;Get start sector
-    mov rbx, qword [rbp + ioReqPkt.bufptr]  ;Get Memory Buffer
+    xor ecx, ecx
+    mov ecx, dword [rbp + bpb32.hiddSec]  ;Goto start of volume
+    add rcx, qword [rdi + ioReqPkt.strtsc]  ;Get sector in volume
+    mov rbx, qword [rdi + ioReqPkt.bufptr]  ;Get Memory Buffer
     mov ah, dh
     mov al, 01h ;Do one sector at a time 
     int 33h
