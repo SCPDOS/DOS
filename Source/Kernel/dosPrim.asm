67f8b7f9415acdcb6fb5a1902515fd34e03fdcf0
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-05-31 18:09:44 +0100

    Added a comment wrt the Int 45h/46h error code translation table as used by DOS

--- Source/Kernel/dosPrim.asm
+++ Source/Kernel/dosPrim.asm
@@ -113,7 +113,8 @@ absDiskReadWriteCommon:
     pop rax
     jz absDiskExit  ;Skip error code checking
     mov al, byte [diskReqHdr + ioReqPkt.status] ;Get low byte into al
-    ;DOS uses the following table
+    ;DOS uses the following pairs in a table
+    ;AH/AL= 80/02, 40/06, 02/0C, 10/04, 04/08, 03/00, 01/80
     mov ah, 80h ;Attachment failure
     cmp al, 0Ch ;Gen error
     je .absExit
