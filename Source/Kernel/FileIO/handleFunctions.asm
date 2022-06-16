35b2c6e706f79e7e8a69a70f5c6ec5f3d1564951
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-06-16 16:04:11 +0100

    Started work on read file

--- Source/Kernel/FileIO/handleFunctions.asm
+++ Source/Kernel/FileIO/handleFunctions.asm
@@ -10,37 +10,37 @@ closeFileHdl:      ;ah = 3Eh, handle function
     ret
 readFileHdl:       ;ah = 3Fh, handle function
     mov byte [rwFlag], 0    ;Read
-    jmp short rwFileHndleCommon
-writeFileHdl:      ;ah = 40h, handle function
-    mov byte [rwFlag], 1    ;Write
-rwFileHndleCommon:
-;bx has file handle, ecx has number of bytes to read
-    mov word [currentHdl], bx
-    call getSFTPtr  ;Get SFT ptr in var in rdi and var
-    jc short lseekHdl.exitBad ;If file handle not good, recycle error
-    ;Now check if device or disk file
-    test word [rdi + sft.wDeviceInfo], devCharDev   ;Is it a char dev?
-    jnz .charDev
-    ;We are a disk (eventually, network too) file.
-    ;Check if we can set the cluster fields first (is dCurntOff <= dFileSize)
-    ;rdi has the SFT pointer
-    mov eax, dword [rdi + sft.dCurntOff]
-    cmp eax, dword [rdi + sft.dFileSize]
-    jae .above
-.above:
-;If it is a read, operation, return 0 bytes.
-    test byte [rwFlag], 1
-    jnz .writeAbove
-    xor eax, eax    ;No characters transferred
-    jmp .exitOK
-.writeAbove:
-.charDev:
-.exitOK:
+    ;bx has file handle, ecx has number of bytes to read
+    ;Set the following vars: currentSFT, currentJFT, currentHdl
+    call getSFTPtr  ;Get SFT ptr in var in rdi
+    jc lseekHdl.exitBad ;If file handle not good, recycle error (in al)
+    ;Now check if we have permissions to read from file
+    movzx eax, word [rdi + sft.wOpenMode]   ;Get handle permissions in ax
+    and al, 0Fh ;Save low nybble
+    cmp al, WriteAccess ;Was this file opened with write access only?
+    jne .readable
+    call invalidFilePermissions
+    ;Set CF on caller stack
     call getUserRegs
-    mov dword [rsi + callerFrame.rax], eax  ;Store number of chars transferred
-    ret
+    or byte [rsi + callerFrame.flags], 1
+    ret ;Exit with error code in al
+.readable:
+    mov eax, dword [rdi + sft.dCurntOff]
+    mov dword [currByteA], eax
+    ;If the file is readable, check if it is a disk or char device
+    movzx eax, word [rdi + sft.wDeviceInfo]
+    test eax, devRedirDev | devCharDev  ;Either of these get handled separately
+    jnz .notDiskDev
+    mov rbp, qword [rdi +sft.qPtr]  ;Get DPB pointer
+    call setWorkingDPB  ;Set the DPB pointer as working
+    mov bl, byte [rbp + dpb.bDriveNumber]
+    mov byte [workingDrv], bl
+.notDiskDev:    ;qPtr here is a device driver
 
+writeFileHdl:      ;ah = 40h, handle function
+    ret
 deleteFileHdl:     ;ah = 41h, handle function, delete from specified dir
+    ret
 lseekHdl:          ;ah = 42h, handle function, LSEEK
 ;New pointer passed in edx! ecx will be DOCUMENTED as having to be 0
     call getSFTPtr
@@ -94,59 +94,12 @@ commitFile:        ;ah = 68h, flushes buffers for handle to disk
 ;-----------------------------------:
 ;        File Handle routines       :
 ;-----------------------------------:
-setFileAccessVariables:
-;This will set up the file access variables and currentSFT 
-; for the SFT pointer in rsi
-;Only used if the SFT is pointing and reading/writing to/from a 
-; hardfile (not device)
-;Uses the file pointer value in the given SFT and the given SFT for
-; computations.
-;Sets up the variables for the SFT AS IT IS WHEN THE FUNCTION IS INVOKED
-;Input: rsi = SFT to setup for
-;Output: Variables initialised:
-;   currentSFT, workingDPB
-;   currClust, currClustA, clustFact, currSect, currSectA, currByte
-;   currByteA
-    push rax
-    push rcx
-    push rdx
-    push rsi
-    push rbp
-;Set current SFT
-    call setCurrentSFT  ;Set rsi the current SFT ptr
-    xor eax, eax
-;Get Disk Relative (absolute) cluster
-    mov eax, dword [rsi + sft.dAbsClusr]
-    mov dword [currClustA], eax 
-;Get File Relative Cluster
-    mov eax, dword [rsi + sft.dRelClust]
-    mov dword [currClust], eax
-;Set working DPB
-    mov rbp, qword [rsi + sft.qPtr] ;Get DPB ptr
-    call setWorkingDPB
-;Get Number of Sectors per Cluster
-    xor eax, eax
-    inc eax
-    mov cl, byte [rbp + dpb.bSectorsPerClusterShift]
-    shl eax, cl ;Get number of Sectors Per Cluster
-    mov byte [clustFact], al 
-;Get Current Byte in File we are pointing to relative to the start of the file
-    mov ecx, dword [rsi + sft.dCurntOff]
-    mov dword [currByteA], ecx
-;___
-; | DO THE FOLLOWING TOGETHER
-; | Get Cluster Relative Sector being pointed to
-; | Get Current Byte in File we are pointing to relative to the sector
-; | Get Disk Relative (absolute) Sector being pointed to
-;_|_
-    call setSectorVars
-    pop rbp
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rax
+invalidFilePermissions:
+    mov eax, errAccDen
+    mov word [errorExCde], ax
+    xor ecx, ecx    ;To allow for using jrcxz to exit
+    stc
     ret
-
 
 setCurrentSFT:
 ;Set the pointer in rsi as current SFT 
@@ -161,7 +114,9 @@ getSFTPtr:
 ;Gets the SFT pointer for a given file handle from the calling application
 ;On entry:
 ;   bx = File handle from JFT for calling application
-;On exit: CF=NC, SFT found and placed in var and rdi
+;On exit: CF=NC, SFT found.
+; Set the following vars: currentSFT, currentJFT, currentHdl
+; Set rdi = currentSFT entry
 ;         CF=CY, SFT not found, abort! al=BadHdl errorcode
     push rax
     push rbx
@@ -169,12 +124,14 @@ getSFTPtr:
     cmp bx, word [maxHndls]  ;current max number of file handles
     jnb .gspFail
     mov rsi, qword [currentPSP]
+    mov eax, ebx    ;Save handle number in eax
     movzx rbx, bx
     lea rbx, qword [rsi + psp.jobFileTbl + rbx] 
     mov [currentJFT], rbx   ;Save a pointer to the JFT entry
     mov bl, byte [rbx]   ;Use jft entry to get sft num
     cmp bl, -1  ;Non-existant SFT reference?
     je .gspFail
+    mov word [currentHdl], ax   ;Current handle number saved here
     xor eax, eax
     mov rdi, qword [sftHeadPtr]
 .gsp0:
@@ -240,64 +197,3 @@ updateCurrentSFT:
     pop rax
     pop rsi
     ret
-
-setClusterVars:
-;Uses the number given in eax as the file pointer, to compute
-; sft fields
-;Works on the SFT pointer provided in rsi
-;Input: rsi = SFT entry pointer
-;Output: rsi = SFT cluster fields updated IF CF=NC
-;       CF=CY => Fail request with Int 44h
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rbp
-;Use variables instead of SFT fields in case the disk fails
-    mov dword [currByteA], eax
-    xor ecx, ecx
-    xor edx, edx
-    mov rbp, qword [rsi + sft.qPtr] ;Get DPB pointer
-    mov cl, byte [rbp + dpb.bBytesPerSectorShift]
-    add cl, byte [rbp + dpb.bSectorsPerClusterShift]
-    ;Get in cl bytes per Cluster shift
-    mov edx, 1
-    shl edx, cl ;Get number of bytes in a cluster in edx
-    mov ecx, edx    ;Move the number of bytes in a cluster to ecx
-    xor edx, edx
-    mov ebx, eax    ;Save byte pointer in ebx
-    div ecx
-    ;eax = Quotient => Relative cluster number
-    ;edx = Remainder => Byte offset into cluster
-    mov dword [currClust], eax    ;Save relative cluster 
-;Now walk the FAT relative cluster number of times
-    mov ecx, eax
-    mov eax, dword [rsi + sft.dStartClust]
-    mov qword [workingDPB], rbp ;Make the dpb working for walkFAT
-.fatWalk:
-    cmp eax, -1
-    je .getFreeSector
-    call walkFAT
-    jc .diskFail
-    dec ecx
-    jnz .fatWalk
-;eax has absolute cluster number now, set SFT fields
-    mov dword [rsi + sft.dAbsClusr], eax
-    mov eax, dword [currClust]
-    mov dword [rsi + sft.dRelClust], eax
-    mov eax, dword [currByteA]
-    mov dword [rsi + sft.dCurntOff], eax
-    clc
-.exit:
-    pop rbp
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-    ret
-.diskFail:
-;FAT read failed, error
-    stc
-    jmp short .exit
-.getFreeSector:
-;Simply need to get the first free sector and add it to the file allocation
