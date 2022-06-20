a63ac87cefeb200f1871a39b44f48584beb528b8
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-06-20 13:26:36 +0100

    Added all Internal DOS Interface Buffer functions Int 4Fh AX=12xx now called Internal DOS Interface (IDI)

--- Source/Kernel/FAT/bufferFunctions.asm
+++ Source/Kernel/FAT/bufferFunctions.asm
@@ -4,16 +4,6 @@
 ;           Externally referenced functions         :
 ;----------------------------------------------------
 
-makeBufferMostRecentlyUsedGetNext: ;Int 4Fh AX=120Fh
-;Sets the buffer in rdi to the head of the chain and gets the 
-; second buffer in the chain in rdi
-;Input: rdi = Buffer header to move to the head of the chain
-;Output: rdi = Second buffer in the chain
-    call makeBufferMostRecentlyUsed
-    mov rdi, qword [bufHeadPtr]
-    mov rdi, qword [rdi + bufferHdr.nextBufPtr]
-    ret
-    
 makeBufferMostRecentlyUsed: ;Int 4Fh AX=1207h
 ;Sets the buffer in rdi to the head of the chain
 ;Input: rdi = Buffer header to move to the head of the chain
@@ -39,219 +29,50 @@ makeBufferMostRecentlyUsed: ;Int 4Fh AX=1207h
 .exit:
     ret
 
-testDirtyBufferForDrive:    ;External linkage
-;Searches the buffer chain for a dirty buffer for a given drive letter.
-;Input: al = Drive number (0 based)
-;Output: CF=CY => Dirty buffer found, CF=NC => No dirty buffer found
-;Consequently, ZF=NZ => Dirty buffer found, ZF=ZE => No dirty buffer found
-    push rbx
-    mov rbx, qword [bufHeadPtr]
-.tdbfdCheckBuffer:
-    cmp byte [rbx + bufferHdr.driveNumber], al
-    jne .tdbfdGotoNextBuffer
-    test byte [rbx + bufferHdr.bufferFlags], dirtyBuffer
-    jz .tdbfdGotoNextBuffer ;Bit not set, goto next buffer
-    stc ;Else dirty buffer found, set carry flag
-.tdbfdExit:
-    pop rbx
-    ret
-.tdbfdGotoNextBuffer:
-    mov rbx, qword [rbx + bufferHdr.nextBufPtr]
-    cmp rbx, -1     ;If rbx points to -1, exit (Also clears CF)
-    je .tdbfdExit
-    jmp short .tdbfdCheckBuffer
-
-freeBuffersForDPB:
-;Walks the buffer chain and sets ALL buffers with the given DPB 
-; to have a drive number of -1, thus freeing it
-;Given DPB is in rbp
-    push rbx
-    mov rbx, qword [bufHeadPtr]
-.i0:
-    cmp qword [rbx + bufferHdr.driveDPBPtr], rbp  ;Chosen DPB?
-    jne .i1 ;If no, skip freeing
-    mov word [rbx + bufferHdr.driveNumber], 00FFh  ;Free buffer and clear flags
-.i1:
-    mov rbx, qword [rbx + bufferHdr.nextBufPtr] ;goto next buffer
-    cmp rbx, -1
-    jne .i0
-.exit:
-    pop rbx
-    ret
-
-
-freeBuffersForDrive:
-;Walks the buffer chain and sets ALL buffers with the given drive number 
-; to have a drive number of -1, thus freeing it
-;Given drive number is in al
-    push rbx
-    mov rbx, qword [bufHeadPtr]
-.i0:
-    cmp byte [rbx + bufferHdr.driveNumber], al  ;Chosen drive?
-    jne .i1 ;If no, skip freeing
-    mov word [rbx + bufferHdr.driveNumber], 00FFh  ;Free buffer and clear flags
-.i1:
-    mov rbx, qword [rbx + bufferHdr.nextBufPtr] ;goto next buffer
-    cmp rbx, -1
-    jne .i0
-.exit:
-    pop rbx
-    ret
-
-
-getBuffer: ;External Linkage (dosPrim.asm, fat.asm)
-;
-;WHENEVER A DATA BUFFER IS NEEDED FOR SECTOR DATA, THIS IS THE FUNCTION
-;TO CALL! WORKS ON THE WORKING DPB!
-;Flushes if not in disk change mode. 
-;If in disk change mode, will check to see if the selected buffer 
-; has data referring a device using the same DPB as we are changing.
-; If it is, skip flushing, if not, flush first!
-;
-;This function will return a pointer to the desired data sector OR 
-; find the most appropriate buffer, flush and read the relevant data into the 
-; buffer, again then returning a pointer to the sector buffer in rbx.
-;Entry: rax = Sector to read
-;        cl = Data type being read (DOS, FAT, DIR, Data)
-;       qword [workingDPB] = DPB to use for transaction
-;Exit:  CF=NC => 
-;           Ptr to buffer header with valid data in buffer in rbx and [currBuf]
-;       CF=CY => Critical Error returned Fail, rbx undefined
-    push rcx
-    push rdx
-    push rsi
-    push rdi
-    mov rsi, qword [workingDPB]  ;Get DPB of transacting device
-    mov dl, byte [rsi + dpb.bDriveNumber]
-    call findSectorInBuffer ;rax = sector to read, dl = drive number
-    cmp rdi, -1
-    je .rbReadNewSector
-.rbExit:
-    clc
-.rbExitNoFlag:
-    pop rdi
-    pop rsi
-    pop rdx
-    pop rcx
-    mov rbx, qword [currBuff]   ;Get current buffer
-    ret
-.rbReadNewSector:
-    call findLRUBuffer  ;Get the LRU or first free buffer entry in rdi
-    cmp byte [diskChange], -1 ;Are we in disk change?
-    jne .flush  ;We are not, flush buffer
-    cmp rsi, qword [rdi + bufferHdr.driveDPBPtr]    ;If yes...
-    je .skipFlush   ;Avoid flushing if same DPB being used
-.flush:
-    call flushAndFreeBuffer
-    jc .rbExitNoFlag    ;Preserve the set carry flag
-.skipFlush:
-;rdi points to bufferHdr that has been appropriately linked to the head of chain
-    mov byte [rdi + bufferHdr.driveNumber], dl
-    mov byte [rdi + bufferHdr.bufferFlags], cl ;FAT/DIR/DATA
-    mov qword [rdi + bufferHdr.bufferLBA], rax
-    cmp cl, fatBuffer
-    mov dl, 1   ;Default values if not fat buffer
-    jne .rbNonFATbuffer
-    mov dl, byte [rsi + dpb.bNumberOfFATs]
-.rbNonFATbuffer:
-    mov byte [rdi + bufferHdr.bufFATcopy], dl
-    mov edx, dword [rsi + dpb.dFATlength]
-    mov dword [rdi + bufferHdr.bufFATsize], edx
-    mov qword [rdi + bufferHdr.driveDPBPtr], rsi
-    mov byte [rdi + bufferHdr.reserved], 0
-    call readSectorBuffer ;Carry the flag from the request
-    jmp short .rbExitNoFlag
-
-;----------------------------------------------------
-;           Internally referenced functions         :
-;----------------------------------------------------
-
-readSectorBuffer:   ;Internal Linkage
-;Reads a sector into a built sector buffer
-;Entry: rdi = Pointer to buffer header
-;Exit:  CF=NC : Success
-;       CF=CY : Fail, terminate the request
-;       rbx pointing to buffer header
-;First make request to device driver
-    push rax
-    push rbx
-    push rcx
-    push rdx
-    push rsi
-    push rbp
-.rsRequest0:
-    mov esi, 3  ;Repeat attempt counter
-    test byte [rdi + bufferHdr.bufferFlags], fatBuffer
-    jz .rsRequest1
-    add esi, 2  ;FAT sectors have 5 attempts
-.rsRequest1:
-    mov al, byte [rdi + bufferHdr.driveNumber]
-    mov ecx, 1  ;One sector to copy
-    mov rdx, qword [rdi + bufferHdr.bufferLBA]
-    mov rbx, qword [rdi + bufferHdr.dataarea]
-    mov rbp, qword [rdi + bufferHdr.driveDPBPtr]
-    call diskReadSetup  ;Setup request (preserves setup registers)
-    call absDiskDriverCall    ;Make Driver Request
-    jnz .rsFail
-.rsExit:
-    clc
-.rsExitFail:
-    pop rbp
-    pop rsi
-    pop rdx
-    pop rcx
-    pop rbx
-    pop rax
-    ret
-.rsFail:
-;Enter here only if the request failed
-    dec esi
-    jnz .rsRequest1 ;Try the request again!
-;Request failed thrice, critical error call
-    mov qword [xInt44RDI], rdi  ;Save rdi
-    mov qword [tmpDPBPtr], rbp  ;Save current DPB ptr here
-    mov edi, eax    ;Save status word in di
-    mov qword [tmpDPBPtr], rbp  ;Save current DPB ptr here
-    mov al, byte [rbp + dpb.bDriveNumber]   ;Get drive number
-    mov ah, critRead | critFailOK | critRetryOK ;Set bits
-    ;Test for correct buffer data type
-    test ah, dosBuffer
-    jnz .rsFail0
-    or ah, critDOS
-    jmp short .rsFailMain
-.rsFail0:
-    test ah, fatBuffer
-    jnz .rsFail1
-    mov ah, critFAT
-    jmp short .rsFailMain
-.rsFail1:
-    test ah, dirBuffer
-    jnz .rsFail2
-    mov ah, critDir
-    jmp short .rsFailMain
-.rsFail2:
-;Here it must be a data buffer
-    mov ah, critData
-.rsFailMain:
-    mov byte [Int44bitfld], ah  ;Save the permissions in var
-    mov rsi, qword [rbp + dpb.qDriverHeaderPtr] ;Get driver header ptr from dpb
-    call criticalDOSError
-    mov rdi, qword [xInt44RDI]
-    mov rbp, qword [tmpDPBPtr]
-    cmp al, critRetry
-    je .rsRequest0
-    ;Else we fail (Ignore=Fail here)
-    mov word [errorExCde], errFI44  ;Replace with Fail on Int 44h
-    stc ;Set error flag to indicate fail
-    jmp .rsExitFail
-
-flushAndFreeBuffer:    ;Internal Linkage Int 4Fh AX=1209h
+flushAndFreeBuffer:         ;Int 4Fh AX=1209h
     call flushBuffer
     jnc .exit
     ;Free the buffer if it was flushed successfully (CF=NC)
     mov word [rdi + bufferHdr.driveNumber], 00FFh   ;Free buffer and clear flags
 .exit:
     ret
+
+markBuffersAsUnreferenced:  ;Int 4Fh AX=120Eh
+;Marks all buffers as unreferenced (clears the reference bit from all buffers)
+;Output: rdi = First disk buffer
+    mov rdi, [bufHeadPtr]
+    push rdi
+.lp:
+    and byte [rdi + bufferHdr.bufferFlags], ~refBuffer
+    mov rdi, qword [rdi + bufferHdr.nextBufPtr]
+    cmp rdi, -1 ;End of chain?
+    jne .lp
+    pop rdi
+    ret
+
+makeBufferMostRecentlyUsedGetNext: ;Int 4Fh AX=120Fh
+;Sets the buffer in rdi to the head of the chain and gets the 
+; second buffer in the chain in rdi
+;Input: rdi = Buffer header to move to the head of the chain
+;Output: rdi = Second buffer in the chain
+    call makeBufferMostRecentlyUsed
+    mov rdi, qword [bufHeadPtr]
+    mov rdi, qword [rdi + bufferHdr.nextBufPtr]
+    ret
+
+findUnreferencedBuffer: ;Int 4Fh AX=1210h
+;Finds the first unreferenced buffer starting at the given buffer header.
+;Input: rdi = Buffer header to start searching at
+;Output: ZF=NZ => rdi = Unreferenced Buffer Header
+;        ZF=ZE => No unreferenced buffer found
+    test byte [rdi + bufferHdr.bufferFlags], refBuffer
+    jnz .exit
+    mov rdi, qword [rdi + bufferHdr.nextBufPtr]
+    cmp rdi, -1
+    jne findUnreferencedBuffer  ;Check next buffer unless rdi = -1
+.exit:
+    ret
+
 flushBuffer:         ;Internal Linkage Int 4Fh AX=1215h
 ;Flushes the data in a sector buffer to disk!
 ;Entry: rdi = Pointer to buffer header for this buffer
@@ -342,6 +163,189 @@ flushBuffer:         ;Internal Linkage Int 4Fh AX=1215h
     mov word [errorExCde], errFI44  ;Replace with Fail on Int 44h
     stc ;Set error flag to indicate fail
     jmp .fbExitFail
+
+
+testDirtyBufferForDrive:    ;External linkage
+;Searches the buffer chain for a dirty buffer for a given drive letter.
+;Input: al = Drive number (0 based)
+;Output: CF=CY => Dirty buffer found, CF=NC => No dirty buffer found
+;Consequently, ZF=NZ => Dirty buffer found, ZF=ZE => No dirty buffer found
+    push rbx
+    mov rbx, qword [bufHeadPtr]
+.tdbfdCheckBuffer:
+    cmp byte [rbx + bufferHdr.driveNumber], al
+    jne .tdbfdGotoNextBuffer
+    test byte [rbx + bufferHdr.bufferFlags], dirtyBuffer
+    jz .tdbfdGotoNextBuffer ;Bit not set, goto next buffer
+    stc ;Else dirty buffer found, set carry flag
+.tdbfdExit:
+    pop rbx
+    ret
+.tdbfdGotoNextBuffer:
+    mov rbx, qword [rbx + bufferHdr.nextBufPtr]
+    cmp rbx, -1     ;If rbx points to -1, exit (Also clears CF)
+    je .tdbfdExit
+    jmp short .tdbfdCheckBuffer
+
+freeBuffersForDPB:
+;Walks the buffer chain and sets ALL buffers with the given DPB 
+; to have a drive number of -1, thus freeing it
+;Given DPB is in rbp
+    push rbx
+    mov rbx, qword [bufHeadPtr]
+.i0:
+    cmp qword [rbx + bufferHdr.driveDPBPtr], rbp  ;Chosen DPB?
+    jne .i1 ;If no, skip freeing
+    mov word [rbx + bufferHdr.driveNumber], 00FFh  ;Free buffer and clear flags
+.i1:
+    mov rbx, qword [rbx + bufferHdr.nextBufPtr] ;goto next buffer
+    cmp rbx, -1
+    jne .i0
+.exit:
+    pop rbx
+    ret
+
+getBuffer: ;External Linkage (dosPrim.asm, fat.asm)
+;
+;WHENEVER A DATA BUFFER IS NEEDED FOR SECTOR DATA, THIS IS THE FUNCTION
+;TO CALL! WORKS ON THE WORKING DPB!
+;Flushes if not in disk change mode. 
+;If in disk change mode, will check to see if the selected buffer 
+; has data referring a device using the same DPB as we are changing.
+; If it is, skip flushing, if not, flush first!
+;
+;This function will return a pointer to the desired data sector OR 
+; find the most appropriate buffer, flush and read the relevant data into the 
+; buffer, again then returning a pointer to the sector buffer in rbx.
+;Entry: rax = Sector to read
+;        cl = Data type being read (DOS, FAT, DIR, Data)
+;       qword [workingDPB] = DPB to use for transaction
+;Exit:  CF=NC => 
+;           Ptr to buffer header with valid data in buffer in rbx and [currBuf]
+;       CF=CY => Critical Error returned Fail, rbx undefined
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    mov rsi, qword [workingDPB]  ;Get DPB of transacting device
+    mov dl, byte [rsi + dpb.bDriveNumber]
+    call findSectorInBuffer ;rax = sector to read, dl = drive number
+    cmp rdi, -1
+    je .rbReadNewSector
+.rbExit:
+    clc
+.rbExitNoFlag:
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    mov rbx, qword [currBuff]   ;Get current buffer
+    ret
+.rbReadNewSector:
+    call findLRUBuffer  ;Get the LRU or first free buffer entry in rdi
+    cmp byte [diskChange], -1 ;Are we in disk change?
+    jne .flush  ;We are not, flush buffer
+    cmp rsi, qword [rdi + bufferHdr.driveDPBPtr]    ;If yes...
+    je .skipFlush   ;Avoid flushing if same DPB being used
+.flush:
+    call flushAndFreeBuffer
+    jc .rbExitNoFlag    ;Preserve the set carry flag
+.skipFlush:
+;rdi points to bufferHdr that has been appropriately linked to the head of chain
+    mov byte [rdi + bufferHdr.driveNumber], dl
+    mov byte [rdi + bufferHdr.bufferFlags], cl ;FAT/DIR/DATA
+    mov qword [rdi + bufferHdr.bufferLBA], rax
+    cmp cl, fatBuffer
+    mov dl, 1   ;Default values if not fat buffer
+    jne .rbNonFATbuffer
+    mov dl, byte [rsi + dpb.bNumberOfFATs]
+.rbNonFATbuffer:
+    mov byte [rdi + bufferHdr.bufFATcopy], dl
+    mov edx, dword [rsi + dpb.dFATlength]
+    mov dword [rdi + bufferHdr.bufFATsize], edx
+    mov qword [rdi + bufferHdr.driveDPBPtr], rsi
+    mov byte [rdi + bufferHdr.reserved], 0
+    call readSectorBuffer ;Carry the flag from the request
+    jmp short .rbExitNoFlag
+
+readSectorBuffer:   ;Internal Linkage
+;Reads a sector into a built sector buffer
+;Entry: rdi = Pointer to buffer header
+;Exit:  CF=NC : Success
+;       CF=CY : Fail, terminate the request
+;       rbx pointing to buffer header
+;First make request to device driver
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rbp
+.rsRequest0:
+    mov esi, 3  ;Repeat attempt counter
+    test byte [rdi + bufferHdr.bufferFlags], fatBuffer
+    jz .rsRequest1
+    add esi, 2  ;FAT sectors have 5 attempts
+.rsRequest1:
+    mov al, byte [rdi + bufferHdr.driveNumber]
+    mov ecx, 1  ;One sector to copy
+    mov rdx, qword [rdi + bufferHdr.bufferLBA]
+    mov rbx, qword [rdi + bufferHdr.dataarea]
+    mov rbp, qword [rdi + bufferHdr.driveDPBPtr]
+    call diskReadSetup  ;Setup request (preserves setup registers)
+    call absDiskDriverCall    ;Make Driver Request
+    jnz .rsFail
+.rsExit:
+    clc
+.rsExitFail:
+    pop rbp
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
+    ret
+.rsFail:
+;Enter here only if the request failed
+    dec esi
+    jnz .rsRequest1 ;Try the request again!
+;Request failed thrice, critical error call
+    mov qword [xInt44RDI], rdi  ;Save rdi
+    mov qword [tmpDPBPtr], rbp  ;Save current DPB ptr here
+    mov edi, eax    ;Save status word in di
+    mov qword [tmpDPBPtr], rbp  ;Save current DPB ptr here
+    mov al, byte [rbp + dpb.bDriveNumber]   ;Get drive number
+    mov ah, critRead | critFailOK | critRetryOK ;Set bits
+    ;Test for correct buffer data type
+    test ah, dosBuffer
+    jnz .rsFail0
+    or ah, critDOS
+    jmp short .rsFailMain
+.rsFail0:
+    test ah, fatBuffer
+    jnz .rsFail1
+    mov ah, critFAT
+    jmp short .rsFailMain
+.rsFail1:
+    test ah, dirBuffer
+    jnz .rsFail2
+    mov ah, critDir
+    jmp short .rsFailMain
+.rsFail2:
+;Here it must be a data buffer
+    mov ah, critData
+.rsFailMain:
+    mov byte [Int44bitfld], ah  ;Save the permissions in var
+    mov rsi, qword [rbp + dpb.qDriverHeaderPtr] ;Get driver header ptr from dpb
+    call criticalDOSError
+    mov rdi, qword [xInt44RDI]
+    mov rbp, qword [tmpDPBPtr]
+    cmp al, critRetry
+    je .rsRequest0
+    ;Else we fail (Ignore=Fail here)
+    mov word [errorExCde], errFI44  ;Replace with Fail on Int 44h
+    stc ;Set error flag to indicate fail
+    jmp .rsExitFail
     
 findLRUBuffer: ;Internal Linkage
 ;Finds first free or least recently used buffer, links it and returns ptr to it 
@@ -397,21 +401,3 @@ findSectorInBuffer:     ;Internal linkage
     cmp rdi, -1     ;If rdi points to -1, exit
     je .fsiExit
     jmp short .fsiCheckBuffer
-
-findDirtyBufferForDrive:    ;No Use
-;Searches the buffer chain for a dirty buffer for a given drive letter.
-;Input: dl = Drive number
-;Output: rbx = Pointer to dirty buffer for drive letter if exists or -1 if not
-    mov rbx, qword [bufHeadPtr]
-.fdbfdCheckBuffer:
-    cmp byte [rbx + bufferHdr.driveNumber], dl
-    jne .fdbfdGotoNextBuffer
-    test byte [rbx + bufferHdr.bufferFlags], dirtyBuffer
-    jz .fdbfdGotoNextBuffer ;Bit not set, goto next buffer
-.fdbfdExit:
-    ret
-.fdbfdGotoNextBuffer:
-    mov rbx, qword [rbx + bufferHdr.nextBufPtr]
-    cmp rbx, -1     ;If rbx points to -1, exit
-    je .fdbfdExit
-    jmp short .fdbfdCheckBuffer
