2f309393d521831e673f4573653dcdc6a8840be7
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-08-06 14:50:22 +0100

    Wrote functions to truncate and totally unlink a FAT chain

--- Source/Kernel/FAT/fat.asm
+++ Source/Kernel/FAT/fat.asm
@@ -234,7 +234,7 @@ getNextSectorOfFile:
     mov byte [currSectC], 0      ;We are at sector 0 rel Clust
     jmp short .exitOK
 
-unlinkfFAT:
+unlinkFAT:
 ;Given a cluster number, will free the cluster and walk the FAT until the first
 ; cluster number considered EOC is found. The given cluster number MUST be
 ; the start cluster of a chain, or at least the cluster linking to the given
