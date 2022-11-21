3530748ddaef8775ad023adad36969ed25812d97
Yll Buzoku <ybuzoku@hotmail.co.uk>
2022-11-21 02:31:35 +0000

    Starting to add final IOCTL. Upgraded to version 0.2

--- Source/Data/staticData.asm
+++ Source/Data/staticData.asm
@@ -3,7 +3,7 @@
 ;       Static Data Variables       :
 ;-----------------------------------:
 dosMajor    db 00h      ;Version 0
-dosMinor    db 01h      ;.01
+dosMinor    db 02h      ;.02
 dosBIOSName db "SCPBIOS .SYS"
 dosKernName db "SCPBDOS .SYS"
 maxHndls    dw 20    ;Initially hardcoded 20, will be made changable soon
--- Source/Kernel/FileIO/ioctl.asm
+++ Source/Kernel/FileIO/ioctl.asm
@@ -11,30 +11,75 @@ ioctrl:            ;ah = 44h, handle function
 ;al = 08h => Is a block device removable?       
 ;al = 09h => Is a logical device local or remote?   
 ;al = 0Ah => Is a handle to a local or remote file? 
-;al = 0Bh => Change sharing retry count (RESERVED)  
+;al = 0Bh => Change sharing retry count 
 ;al = 0Ch => Generic IOCTL handle request (RESERVED) - Code page switching
 ;al = 0Dh => Block device Generic IOCTL request     
 ;al = 0Eh => Get logical device                 
-;al = 0Fh => Set logical device                 
+;al = 0Fh => Set logical device  
+    call derefSFTPtr
+    jnc .handleOk
+    mov eax, errBadHdl
+    jmp extErrExit
+.handleOk:
+;Now rdi has ptr to SFT handle               
     test al, al
     jz .getDevWord
     cmp al, 1
     je .setDevWord
+    cmp al, 2
+    je .readCharDev
+    cmp al, 3
+    je .writeCharDev
+    cmp al, 4
+    je .readBlockDev
+    cmp al, 5
+    je .writeBlockDev
+    cmp al, 6
+    je .getInputStatus
+    cmp al, 7
+    je .getOutputStatus
+    cmp al, 8
+    je .isRemdev
+    cmp al, 9
+    je .isDevLocal
+    cmp al, 0Ah
+    je .isHdlLocal
+    cmp al, 0Bh
+    je .setSharingCount
+    cmp al, 0Ch
+    je .invalidFunction ;Change Codepage, reserved for now
+    cmp al, 0Dh
+    je .genericDevIOCTL
+    cmp al, 0Eh
+    je .getDrvLogicalDevice
+    cmp al, 0Fh
+    je .setDrvLogicalDevice
+.invalidFunction:
     mov al, errInvFnc
     jmp extErrExit
 .getDevWord:
-    ;Input: bx = File Handle
-    call derefSFTPtr
-    jc extErrExit
-    ;Now rdi has ptr to SFT handle
+;Input: bx = File Handle
     mov dx, word [rdi + sft.wDeviceInfo]
     xor al, al
     call getUserRegs
     mov word [rsi + callerFrame.rdx], dx
     return
 .setDevWord:
-    ;Input: bx = File Handle
-    ;       dl = Low byte of Device information word 
-    call derefSFTPtr
+;Input: bx = File Handle
+;       dl = Low byte of Device information word 
     mov byte [rdi + sft.wDeviceInfo], dl
     return
+.readCharDev:
+.writeCharDev:
+.readBlockDev:
+.writeBlockDev:
+.getInputStatus:
+.getOutputStatus:
+.isRemdev:
+.isDevLocal:
+.isHdlLocal:
+.setSharingCount:
+.genericDevIOCTL:
+.getDrvLogicalDevice:
+.setDrvLogicalDevice:
+    jmp extErrExit
