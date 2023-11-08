88b0d823a1a531b10634941329b9ff7a1b658397
ybuzoku <ybuzoku@hotmail.co.uk>
2023-11-08 01:13:54 +0000

    Localisation implementation complete. Default drivers, as in dos, have no IOCTL capabilities, so need to write IOCTL replacements to be dynamically loaded. Since now all kernel functions (except FCB IO functions implemented), upgade to v0.96

--- src/Kernel/Local/local.asm
+++ src/Kernel/Local/local.asm
@@ -56,8 +56,7 @@ getsetCountryInfo: ;ah = 38h, localisation info
     xor ebp, ebp    ;Get country table
     mov ecx, 1      ;Set country table
     cmp rdi, -1     ;Set or Get?
-    cmove ebp, ecx  ;If rdi = -1, it's set, so init ebp to 1
-    je .goToNlsFunc ;If set, immediately goto nls func
+    je .goToNlsFuncSet ;If set, immediately goto nls func
     test ebx, ebx   ;Else, in get, are we looking for current country?
     jnz .goToNlsFunc    ;If not zero, goto nls func
     ;Get current country info here, works w/o NLSFUNC
@@ -72,6 +71,8 @@ getsetCountryInfo: ;ah = 38h, localisation info
 .exitNoCountryCode: 
     mov eax, ebx    ;Move country code into eax for return (undocumented)
     jmp extGoodExit
+.goToNlsFuncSet:
+    mov ebp, ecx    ;Also set ebp to 1
 .goToNlsFunc:
     call .nlsWrap   ;Access NLS functionality
     jc extErrExit   ;If CF=CY, exit error (error code in al)
@@ -86,10 +87,10 @@ getsetCountryInfo: ;ah = 38h, localisation info
 ; for the current country (to avoid hitting NLSFUNC and erroring if
 ; not installed).
 ;Input: ebp = 0 -> Get country info
-;           = 1 -> Set code page
+;           = 1 -> Set DOS country info
 ;        bx = Country code (bx <> 0 here)
 ;       rdi -> User buffer
-;       rsi -> DOS codepage structure
+;       rsi -> DOS internal NLS structure
 ;Output: CF = CY -> Error, al has error code (-1 is generic error)
 ;        CF = NC -> OK
 ;        bx = 0 => Entered NLS and data copied to user buffer.
@@ -103,9 +104,9 @@ getsetCountryInfo: ;ah = 38h, localisation info
     int 4fh
     cmp al, -1      ;If al <> -1, error exit
     jne .errNotInstalled
-    lea rsi, dosCodepage    ;Point rsi to the DOS codepage area
-    mov eax, 1404h  ;Get Country Info, for country code in dx (bx <> codepage)
-    mov ecx, 1403h  ;Set codepage, for country code in dx (bx <> codepage)
+    lea rsi, dosNLSPtr    ;Point rsi to the DOS codepage area
+    mov eax, 1404h  ;Get Country Info, for country code in dx
+    mov ecx, 1403h  ;Set DOS Country Info, to country code in dx
     test ebp, ebp   ;What can I do you for amigo?
     cmovnz eax, ecx ;Set codepage if this is non-zero
     int 4fh
@@ -120,17 +121,101 @@ getsetCountryInfo: ;ah = 38h, localisation info
 
 
 getExtLocalInfo:    ;ah = 65h, Get Extended Country Info
-    mov eax, errAccDen
+;al = info ID
+;   01h get general internationalization info
+;   02h get pointer to uppercase table
+;   04h get pointer to filename uppercase table
+;   05h get pointer to filename terminator table
+;   06h get pointer to collating sequence table
+;bx = code page (FFFFh=global code page)
+;dx = country ID (FFFFh=current country)
+;rdi -> country information buffer
+;ecx = size of buffer (>= 9), fail if less! NOTE THE CHANGE FROM 5 to 9!!!!
+;Return:
+;CF=CY if error
+;ax = error code
+;CF=NC if successful
+;ecx = size of country information returned
+;rdi -> country information filled in
+;Undocumented: ax = default Codepage if nls or requested codepage if internal
+    cmp ecx, 9  ;Is our buffer of minimum acceptable size?
+    jb .invFuncExit
+    lea rsi, dosNLSPtr
+    cmp dx, -1
+    cmove dx, word [extCtryTbl.countryCode] ;Get the current country code
+    cmp bx, -1
+    cmove bx, word [extCtryTbl.activeCP]    ;Get the current codepage
+    cmp dx, word [extCtryTbl.countryCode]
+    jne .nlsReq
+    cmp bx, word [extCtryTbl.activeCP]
+    jne .nlsReq
+    ;We access our local DOS nationalisation tables to 
+    ; avoid hitting NLS.
+    lea rsi, charTableArray
+    movzx ebp, word [ctryFunctions]    ;Get max number of tables to parse
+.loopTableSearch:
+    cmp al, byte [rsi]  ;Is the table ours?
+    jne .tblFound
+    add rsi, 9          ;Go to next table (skip one byte and one qword)
+    dec ebp
+    jnz .loopTableSearch
+    ;Fallthrough to error if no match
+.invFuncExit:
+    mov eax, 1
     jmp extErrExit
-
+.tblFound:
+    movsb   ;Copy over the first byte, moving both pointers by 1
+    cmp al, 1
+    je .getExtCtryTbl
+    ;Here we simply copy over a qword
+    mov ecx, 8  ;Copy 8 more bytes
+    mov eax, 9  ;Number of bytes to save as having had been written
+.copyTable:
+;Come here with eax = full copy len, ebx = codepage value, ecx = bytes to xfr
+    rep movsb   ;Copy the rest of the bytes
+    call getUserRegs
+    mov dword [rsi + callerFrame.rcx], eax    ;Store in ecx # of bytes
+    ;Undocumented, if success, ax contains the requested codepage
+    ; value. This is undocumented and should not be relied upon.
+    mov eax, ebx    
+    jmp extGoodExit
+.getExtCtryTbl:
+;ecx has length of caller buffer
+    sub ecx, 3  ;Remove the byte from the count and the length we will store
+    xor eax, eax    ;Clear eax
+    lodsw           ;Get word at rsi (full table len) and advance rsi by 2
+    cmp eax, ecx    ;Do we have more bytes in buffer than we need?
+    cmova ecx, eax  ;Set it to exactly the table length in that case
+    mov eax, ecx    ;Copy the number of bytes back to eax too
+    stosw           ;Store number of bytes we will copy and adv rdi by 2
+    lea eax, dword [ecx + 3] ;Add 3 to eax, for first 3 bytes copied
+    jmp short .copyTable
+    
+.nlsReq:    ;nlsReq moved here to reach the short jumps :)
+;As before, now rsi -> DOSNLS structure
+; and bpl has the function code (1,2,4,5,6)
+    movzx ebp, al   ;Place the function code in ebp (low byte, zx the rest)
+    mov eax, 1400h  ;Install check!
+    int 4fh
+    cmp al, -1          ;If not installed, error exit
+    jne .invFuncExit    
+    mov eax, 1402h      ;Get codepage info
+    int 4fh
+    test al, al         ;If the return code is 0, we are ok, else
+    jne extErrExit      ; al has error code
+    ;Undocumented, if success, ax contains the default codepage
+    ; value. This is undocumented and should not be relied upon.
+    ;This value never changes, not even by NLSFUNC.
+    movzx eax, word [defaultCP]
+    jmp extGoodExit
 
 getsetGlobalCP:    ;ah = 66h, Get/Set Global Codepage
 ;If al = 01h -> Get Global Codepage
 ;Return:    ebx = Active (current) codepage
 ;           edx = System (default) codepage
 ;If al = 02h -> Set Global Codepage
-;   (e)bx = Active (current) codepage
-;   (e)dx = System (default) codepage. Not needed, so don't document.
+;   bx = Active (current) codepage
+;   dx = System (default) codepage. Not needed, so don't document.
     cmp al, 1
     jne .setCodepage
 ;Here we get the codepage
@@ -151,8 +236,8 @@ getsetGlobalCP:    ;ah = 66h, Get/Set Global Codepage
     int 4Fh
     cmp al, -1
     jne .exitBadFunc
-    lea rsi, dosCodepage    ;Get pointer to the DOS codepage in rsi
-    mov eax, 1401h  ;Set global codepage
+    lea rsi, dosNLSPtr  ;Get pointer to the DOS codepage in rsi
+    mov eax, 1401h      ;Set global codepage
     int 4Fh
     test al, al
     jz extGoodExit
