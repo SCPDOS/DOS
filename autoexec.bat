ff4a77ba4dcc065cac5e1a5d963c8071ab42b658
ybuzoku <ybuzoku@hotmail.co.uk>
2024-05-06 19:25:41 +0100

    Adding two batch files for building disk images

---
+++ autoexec.bat
@@ -0,0 +1,5 @@
+@echo off
+path=a:\
+c:
+cls
+dir
---
+++ instc.bat
@@ -0,0 +1,19 @@
+@echo off
+echo Installing SCP/DOS on C:
+format c:
+sys c:
+c:
+mklbl
+a:
+md c:dos
+@echo on
+copy *.com c:dos
+copy *.exe c:dos
+echo @echo off > c:\autoexec.bat
+echo path=c:\dos >> c:\autoexec.bat
+echo cls >> c:\autoexec.bat
+echo dir >> c:\autoexec.bat
+echo buffers=99 > c:\config.sys
+echo files=254 >> c:\config.sys
+echo lastdrive=z >> c:\config.sys
+echo Install complete
