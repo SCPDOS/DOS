4ff34bdfab0c52168efa10a2bf8d2602e7e22a73
ybuzoku <ybuzoku@hotmail.co.uk>
2025-02-03 02:12:27 +0000

    Updated disk images with new BIOS.
    Unformatted media now returns a safe number of sectors for max count.

--- src/Drivers/diskDrv.asm
+++ src/Drivers/diskDrv.asm
@@ -993,6 +993,7 @@ msdDriver:
     mov eax, 8800h  ;Read LBA Device Parameters
     int 33h
     jc .errorXlat
+    sub rcx, 2      ;Drop two sectors from the absolute LBA count
     xor edx, edx    ;0 Hidden sectors on remdevs/unformatted media
     jmp short .lgpStor
 .lgpbpbok:
